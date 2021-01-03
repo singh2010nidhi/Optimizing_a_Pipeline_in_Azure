d432e1759f9a3447c663fce9bbb1394656424446
Nidhi Singh <singh2010nidhi@gmail.com>
2021-01-03 08:50:41 +0000

    modify train.py

--- train.py
+++ train.py
@@ -14,15 +14,7 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
+ds = TabularDatasetFactory.from_delimited_files(path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv");
 
 def clean_data(data):
     # Dict for cleaning data
@@ -49,7 +41,16 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+
+    return x_df, y_df;
     
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+
+x_train , x_test, y_train, y_test = train_test_split(x, y, train_size = 0.80, test_size = 0.20, random_state = 42)
+
+run = Run.get_context()
 
 def main():
     # Add arguments to script
@@ -66,7 +67,10 @@ def main():
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
+    os.makedirs('outputs', exist_ok = "True")
+    joblib.dump(model,'outputs/model.joblib')
     run.log("Accuracy", np.float(accuracy))
+
 
 if __name__ == '__main__':
     main()
